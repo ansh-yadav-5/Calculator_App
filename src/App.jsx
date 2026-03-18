e60989678938eb5225c5b62c33f83fe1358b2b96
Ansh Yadav <ansh@example.com>
2026-03-19 00:31:07 +0530

    Complete the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import Display from "./components/Display";
+import styles from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
 
 function App() {
-  
   return (
-    <>
-      <h1>Calculator</h1>
-    </>
-  )
+    <div className={styles.calculator}>
+      <Display></Display>
+      <ButtonsContainer></ButtonsContainer>
+    </div>
+  );
 }
 
-export default App
+export default App;
