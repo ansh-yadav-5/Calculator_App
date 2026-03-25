8445f41383b9db036a09a4ad34755de31eca26ea
Ansh Yadav <ansh@example.com>
2026-03-25 23:47:36 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Display from "./components/Display";
 import styles from "./App.module.css";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 
 function App() {
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result.toString());
+    }else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  }
+   
+
   return (
     <div className={styles.calculator}>
-      <Display></Display>
-      <ButtonsContainer></ButtonsContainer>
+      <h1 className={styles.title}>Calculator</h1>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
     </div>
   );
 }
